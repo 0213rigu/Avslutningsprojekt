58bec09d16841b1553599f19e94884d96a9c143a
0213rigu <0213rigu@jonkoping.se>
2021-05-23 20:13:07 +0200

    Färdig sida med grafer skapade med hjälp av chartjs

--- Progress/prgrs.js
+++ Progress/prgrs.js
@@ -3,24 +3,24 @@ var goalprogress = 0;
 var procenttxt = 0;
 var goalrange = 0;
 var goalunit = "unit";
- function prgrscalc() {
-    document.getElementById("prgrsfill").style.strokeDashoffset = "calc( 440 - (440 *" + procent + ")" ;
-    procenttxt = Math.round(procent*100);
+let addNmbr= 0;
+
+function prgrscalc() {
+    document.getElementById("prgrsfill").style.strokeDashoffset = "calc( 440 - (440 *" + procent + ")";
+    procenttxt = Math.round(procent * 100);
     document.getElementById("percentnmbr").innerHTML = procenttxt;
     document.getElementById("goalprogress").innerHTML = goalprogress + "/" + goalrange + " " + goalunit;
-    }
+}
 
 window.onload = prgrscalc;
 
 
-function collectvalue(){
+function collectvalue() {
     var goalname = document.getElementById("goal").value;
     document.getElementById("goalname").innerHTML = goalname;
     goalrange = document.getElementById("goalnmbr").value;
     goalunit = document.getElementById("goalunit").value;
-    document.getElementById("goalprogress").innerHTML = "0/" +  goalrange + " " + goalunit;
-   /* document.getElementById("goalnmbr").style.display = "none";
-    document.getElementById("nmbrlbl").style.display="none";*/
+    document.getElementById("goalprogress").innerHTML = "0/" + goalrange + " " + goalunit;
     procent = 0;
     prgrscalc();
     $("#unitlbl").remove();
@@ -32,27 +32,77 @@ function collectvalue(){
 
     document.getElementById("nmbrlbl").innerHTML = "Add progress";
 
+    addData();
+}
+//Grafer
+let chartData = [0];
+let chartName = ["addition 0"];
+
+function addprgrs() {
+    addNmbr++;
+    let xaxis = "Addition " + addNmbr;
+    chartName.push(xaxis);
+    console.log(chartName);
+    if (procent < 1) {
+        let addval = document.getElementById("goalnmbr").value;
+        goalprogress = parseInt(goalprogress) + parseInt(addval);
+        procent = goalprogress / goalrange;
+        if (procent<1){
+            chartData.push(goalprogress);
+            prgrscalc();
+            console.log(chartData);
+            addData();
+        }
+        if (procent >= 1) {
+            procent = 1;
+            goalprogress = parseInt(goalrange);
+            chartData.push(goalprogress);
+            prgrscalc();
+            console.log(chartData);
+            addData();
+        }
+    } else {
+        procent = 1;
+        goalprogress = goalrange;
+        prgrscalc();
+
+    }
 }
 
+let myChart = document.getElementById("myChart").getContext('2d');
 
-function addprgrs( ){
-    if(procent<1){
-    let addval = document.getElementById("goalnmbr").value;
-    goalprogress = parseInt(goalprogress) + parseInt(addval);
-    procent = goalprogress/goalrange;
-    console.log(procent);
-    prgrscalc();
-    if(procent>1){
-        procent=1;
-        goalprogress=goalrange;
-        prgrscalc();
-    }
-    }
-    else{
-        procent = 1;    
-        goalprogress=goalrange;
-        prgrscalc();
-    }
+function addData() {
+    
+    document.getElementById("chartDiv").style.display="flex";
+    lineChart.data.datasets[0].data = chartData;
+    lineChart.data.labels = chartName;
+    lineChart.options.scales.y.suggestedMax = goalrange;
+    lineChart.update();
+    
 }
+let lineChart = new Chart(myChart, {
+    type: 'line',
+    data: {
+        labels: [
+        ],
+        datasets: [{
+
+            label: 'Goal Progress',
+
+            data: [
+            ],
+
+            borderColor: 'purple',
 
 
+        }]
+
+    },
+    options: {
+        scales : {
+            y:{
+                suggestedMin: 0,
+            }
+        }
+    }
+});
--- nav.js
+++ nav.js
@@ -13,7 +13,7 @@ window.onload = function () {
     else {
 
     }
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+    $('#nav-icon1').click(function(){
 		$(this).toggleClass('open');
         document.getElementById('linkDiv').classList.toggle('open');
 	});
@@ -27,16 +27,15 @@ window.onload = function () {
 }
 function scrollFunction() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        document.getElementById("navbar").style.height = "9vh";
+        document.getElementById("linkDiv").style.height = "9vh";
         document.getElementById("navbar-a1").style.cssText = "font-size: 200%; color: rgba(255,255,255,0.3) ";
         document.getElementById("navbar-a2").style.cssText = "font-size: 200%; color: rgba(255,255,255,0.3) ";
-        document.getElementById("navbar-a3").style.cssText = "font-size: 200%; color: rgba(255,255,255,0.3) ";
+
 
     } else {
-        document.getElementById("navbar").style.height = "20vh";
         document.getElementById("navbar-a1").style.cssText = "font-size: 400%; color: rgba(255,255,255,1) ";
         document.getElementById("navbar-a2").style.cssText = "font-size: 400%; color: rgba(255,255,255,1) ";
-        document.getElementById("navbar-a3").style.cssText = "font-size: 400%; color: rgba(255,255,255,1) ";
+
     }
 
 }
